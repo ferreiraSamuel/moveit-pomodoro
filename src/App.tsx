d5f49c36f312c3080c60d2020f81ad0f51c1b245
Samuel Ferreira <samuelfbjm@gmail.com>
2021-02-28 10:54:09 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+//components
+import { ExperienceBar } from './components/ExperienceBar'
+
+//styles 
+import './styles/global.css'
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
